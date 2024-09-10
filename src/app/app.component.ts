e54c2542be982175b9f54e25027818064a4cc95d
Demi Lune <anshikathakur596@gmail.com>
2024-09-10 15:41:08 +0530

    imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TallyboxComponent } from './tallybox/tallybox.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TallyboxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
